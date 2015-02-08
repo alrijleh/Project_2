f370f7efebb47286fd4f50d4539c2a75b111c0c1
Fouad Al-Rijleh <alrijleh@gmail.com>
2015-02-08 10:42:41 -0500

    cleaned out main files

--- Project 2/main.cpp
+++ Project 2/main.cpp
@@ -3,13 +3,5 @@ using namespace std;
 
 void main()
 {
-	vector<int> myVec(15);
-	for (int i = 0; i < myVec.size(); i++)
-	{
-		myVec[i] = rand() % 50;
-	}
-	cout << myVec << endl;
-	quickSort(myVec);
-	cout << myVec << endl;
-	system("pause");
+
 }
