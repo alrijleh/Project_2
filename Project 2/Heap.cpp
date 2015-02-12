5b9ffd4ef41fba67770512dd5d93d16d42f530bb
Rachel <rudolph.r@husky.neu.edu>
2015-02-12 16:00:02 -0500

    maxHeapify, buildMaxHeap and heapSort functions.

--- Project 2/Heap.cpp
+++ Project 2/Heap.cpp
@@ -1,72 +1,99 @@
 #include "Heap.h"
 
 template <typename T>
-Heap::Heap()
+Heap<T>::Heap()
 {
 }
 
 template <typename T>
-Heap::~Heap()
+Heap<T>::~Heap()
 {
 }
 
 template <typename T>
-void setVector(vector<T> newVector)
+void Heap<T>::setVector(vector<T> newVector)
 {
 
 }
 
 
 template <typename T>
-vector<T> getVector() const
-{
-
-}
-
-template <typename T>
-int getItem(int i) const
+vector<T> Heap<T>::getVector() const
 {
 
 }
 
 template <typename T>
-int left(int i) const
+int Heap<T>::getItem(int i) const
 {
 
 }
 
 template <typename T>
-int right(int i) const
+int Heap<T>::left(int i) const
 {
 
 }
 
 template <typename T>
-int parent(int i) const
+int Heap<T>::right(int i) const
 {
 
 }
 
 template <typename T>
-void initializeMaxHeap()
+int Heap<T>::parent(int i) const
 {
 
 }
 
 template <typename T>
-void maxHeapify(int i)
+void Heap<T>::initializeMaxHeap()
 {
 
 }
 
 template <typename T>
-void buildMaxHeap()
+void Heap<T>::maxHeapify(int i)
 {
-
+	int l = left(i);
+	int r = right(i);
+	int largest; 
+	if (l <= vector.size() && vector[l] > vector[i])
+	{
+		largest = l;
+	}
+	else largest = i;
+	if (r <= vector.size() && vector[r] > vector[largest])
+	{
+		largest = r;
+	}
+	if (largest != i)
+	{
+		swap(vector[i], vector[largest]);
+		maxHeapify(vector, largest);
+	}
 }
 
 template <typename T>
-void heapSort()
+void Heap<T>::buildMaxHeap()
 {
+	int heapSize = vector.size();
+	for (int index = heapSize / 2; i >= 1; i--)
+	{
+		maxHeapify(vector[i]);
+	}
+}
 
+template <typename T>
+void Heap<T>::heapSort()
+{
+	buildMaxHeap(vector);
+	int lastIndex = vector.size() - 1;
+	for (int index = vector.size(); i >= 2; i--)
+	{
+		swap(vector[lastIndex], vector[index]);
+		vector.resize(vector.size() - 1);
+		maxHeapify(vector, lastIndex);
+	}
 }
