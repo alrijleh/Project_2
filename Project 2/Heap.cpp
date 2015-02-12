e6483011012c242f819ccf6bb2e5acff05c48f27
Fouad Al-Rijleh <alrijleh@gmail.com>
2015-02-12 15:44:21 -0500

    added Heap::
    made setters/getters
    made left,right,parent

--- Project 2/Heap.cpp
+++ Project 2/Heap.cpp
@@ -1,72 +1,74 @@
 #include "Heap.h"
 
 template <typename T>
-Heap::Heap()
+Heap<T>::Heap()
 {
 }
 
 template <typename T>
-Heap::~Heap()
+Heap<T>::~Heap()
 {
 }
 
 template <typename T>
-void setVector(vector<T> newVector)
+void Heap<T>::setVector(vector<T> newVector)
 {
-
+	vector = newVector;
 }
 
-
-template <typename T>
-vector<T> getVector() const
+template<typename T>
+vector<T> Heap<T>::getVector() const
 {
-
-}
-
-template <typename T>
-int getItem(int i) const
-{
-
+	vector = newVector;
 }
 
 template <typename T>
-int left(int i) const
+int Heap<T>::getItem(int i) const
 {
+	return vector[i + 1]
+}
 
+//returns the left child to item i
+template <typename T>
+int Heap<T>::left(int i) const
+{
+	return i * 2;
+}
+
+//returns the right child to item i
+template <typename T>
+int Heap<T>::right(int i) const
+{
+	return i * 2 + 1;
+}
+
+//returns the parent to item i
+template <typename T>
+int Heap<T>::parent(int i) const
+{
+	return i / 2;
 }
 
 template <typename T>
-int right(int i) const
-{
-
-}
-
-template <typename T>
-int parent(int i) const
-{
-
-}
-
-template <typename T>
-void initializeMaxHeap()
-{
-
-}
-
-template <typename T>
-void maxHeapify(int i)
+void Heap<T>::initializeMaxHeap()
 {
 
 }
 
 template <typename T>
-void buildMaxHeap()
+void Heap<T>::maxHeapify(int i)
 {
 
 }
 
 template <typename T>
-void heapSort()
+void Heap<T>::buildMaxHeap()
+{
+
+}
+
+template <typename T>
+void Heap<T>::heapSort()
 {
 
 }
