eee27f5e967207bbfb300d405a8c7653d02d7cc9
Fouad Al-Rijleh <alrijleh@gmail.com>
2015-02-19 16:23:05 -0500

    Created functions for Hashtable.h
    functions are currently untested

--- Project 2/Hashtable.h
+++ Project 2/Hashtable.h
@@ -1,5 +1,7 @@
 #pragma 
 
+#include "SearchSort.h"
+#include "d_except.h"
 #include <iostream>
 #include <vector>
 
@@ -8,41 +10,52 @@ template<typename T>
 class Hashtable
 {
 private: 
-	vector<vector<T>> vector; 
+	vector<vector<T>> table; 
 
 public:
 	Hashtable();
-	Hashtable(vector<vector<T>> vector);
+	Hashtable(vector<vector<T>> newtable);
 	~Hashtable();
 
-	void setVector(vector<vector<T>> newVector)
+	void setTable(vector<vector<T>> newtable)
 	{
-		vector = newVector;
+		table = newTable;
 	}
 
-	vector<vector<T>> getVector() const
+	vector<vector<T>> getTable() const
 	{
-		vector = newVector;
+		table = newtable;
 	}
 
-	int addItem(int i) const
+	void addItem(T item) const
 	{
-		
+		int slot;
+		slot = hash(item);
+		table[slot].push_back(item)
 	}
 
-	int deleteItem(int i) const
+	void deleteItem(T item) const
 	{
-		
+		int slot, location;
+		slot = hash(item);
+		location = SearchSort::linearSearch(table[slot], item);
+		if (location == -1) throw referenceError("item not found in hash table");
+		else table[slot].erase(table[slot].begin() + location);
 	}
 
-	int inList(int i) const
+	bool inList(T item) const
 	{
-
+		int slot, location;
+		slot = hash(item);
+		location = SearchSort::linearSearch(table[slot], item);
+		if (location == -1) return false;
+		else return true;
 	}
 
-	void hash()
+	int hash(T item)
 	{
-
+		std::hash<T> hash_struct;
+		return hash_struct(item) % table.size();
 	}
 };
 
