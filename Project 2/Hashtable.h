912e6d8e391fc9f7e8acf1d5da50f319a6d8ff21
Fouad Al-Rijleh <alrijleh@gmail.com>
2015-02-22 19:43:24 -0500

    seperated the declarations and definitions within the hashtable class

--- Project 2/Hashtable.h
+++ Project 2/Hashtable.h
@@ -14,76 +14,99 @@ private:
 
 public:
 	Hashtable(){}
-	Hashtable(vector<T> baseVector)
-	{
-		Hashtable(baseVector, baseVector.size());
-	}
-	Hashtable(vector<T> baseVector, int numSlots)
-	{
-		vector<vector<T>> table(numSlots);
-		for (int index = 0; index < baseVector.size(); index++)
-		{
-			addItem(baseVector[index]);
-		}
-	}
-
+	Hashtable(vector<T> baseVector);
+	Hashtable(vector<T> baseVector, int numSlots);
 	~Hashtable(){}
 
-	void setTable(vector<vector<T>> newtable)
-	{
-		table = newTable;
-	}
+	void setTable(vector<vector<T>> newtable);
+	vector<vector<T>> getTable() const;
 
-	vector<vector<T>> getTable() const
-	{
-		return table;
-	}
-	
-	int getSlotCount() const
-	{
-		return table.size();
-	}
+	int getSlotCount() const;
+	vector<int> getSlotSizes() const;
 
-	vector<int> getSlotSizes() const
+	void addItem(T item);
+	void deleteItem(T item);
+	bool inList(T item) const;
+	int hash(T item) const;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+template<typename T>
+Hashtable::Hashtable(vector<T> baseVector)
+{
+	Hashtable(baseVector, baseVector.size());
+}
+
+template<typename T>
+Hashtable::Hashtable(vector<T> baseVector, int numSlots)
+{
+	vector<vector<T>> table(numSlots);
+	for (int index = 0; index < baseVector.size(); index++)
 	{
-		vector<int> sizeVector(table.size())
+		addItem(baseVector[index]);
+	}
+}
+
+template<typename T>
+void Hashtable::setTable(vector<vector<T>> newtable)
+{
+	table = newTable;
+}
+
+template <typename T>
+vector<vector<T>> Hashtable::getTable() const
+{
+	return table;
+}
+
+template <typename T>
+int Hashtable::getSlotCount() const
+{
+	return table.size();
+}
+
+template <typename T>
+vector<int> getSlotSizes() const
+{
+	vector<int> Hashtable::sizeVector(table.size())
 		for (int index = 0; index < table.size(); index++)
 		{
 			sizeVector[index] = table[index].size();
 		}
-		return sizeVector;
-	}
+	return sizeVector;
+}
 
-	void addItem(T item)
-	{
-		int slot;
-		slot = hash(item);
-		table[slot].push_back(item);
-	}
+template <typename T>
+void Hashtable::addItem(T item)
+{
+	int slot;
+	slot = hash(item);
+	table[slot].push_back(item);
+}
 
-	void deleteItem(T item)
-	{
-		int slot, location;
-		slot = hash(item);
-		location = SearchSort::linearSearch(table[slot], item);
-		if (location == -1) throw referenceError("item not found in hash table");
-		else table[slot].erase(table[slot].begin() + location);
-	}
+template <typename T>
+void Hashtable::deleteItem(T item)
+{
+	int slot, location;
+	slot = hash(item);
+	location = SearchSort::linearSearch(table[slot], item);
+	if (location == -1) throw referenceError("item not found in hash table");
+	else table[slot].erase(table[slot].begin() + location);
+}
 
-	bool inList(T item) const
-	{
-		int slot, location;
-		slot = hash(item);
-		location = SearchSort::linearSearch(table[slot], item);
-		if (location == -1) return false;
-		else return true;
-	}
+template <typename T>
+bool Hashtable::inList(T item) const
+{
+	int slot, location;
+	slot = hash(item);
+	location = SearchSort::linearSearch(table[slot], item);
+	if (location == -1) return false;
+	else return true;
+}
 
-	int hash(T item) const
-	{
-		std::hash<T> hash_struct;
-		return hash_struct(item) % table.size();
-	}
-};
-
-
+template <typename T>
+int Hashtable::hash(T item) const
+{
+	std::hash<T> hash_struct;
+	return hash_struct(item) % table.size();
+}
